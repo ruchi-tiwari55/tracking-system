d0f06d078b7654bbf9c39c3133a0b6c5bb698c34
alkesh <alkeshpatidar946@gmail.com>
2024-05-01 18:23:34 +0530

    minor changes

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -7,16 +7,18 @@ const Footer = () => {
   return (
     <div className='vh-9'>
       <div className={styles.ftop}>India</div>
-      <div className={styles.fdown}>
-        <div className={styles.fleft} style={{gap:10}}>
+      <div className={styles.fdown} style={{}}>
+        <div className={styles.fleft} style={{ gap: 10 }}>
           <Link to="/about"><p>About</p></Link>
           <Link to="/advertising"><p>Advertising</p></Link>
           <Link to="/business-profile"><p>Business</p></Link>
           <Link to="/investors"><p>Investor</p></Link>
           <Link to="/hiring"><p>Team</p></Link>
-          <Link to="/privacy-policy"><p>Privacy & Terms</p></Link>
-          <Link to="/help-center"><p>Help</p></Link>
         </div>
+        <div className={styles.fright}>
+            <Link to="/privacy-policy"><p>Privacy & Terms</p></Link>
+            <Link to="/help-center"><p>Help</p></Link>
+          </div>
       </div>
     </div>
   )
