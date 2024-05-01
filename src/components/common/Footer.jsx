46fac1adf1777ac2b182d84ed4c7252f6f0970e8
alkesh <alkeshpatidar946@gmail.com>
2024-05-01 14:06:28 +0530

    footer and login responsiveness

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -8,14 +8,12 @@ const Footer = () => {
     <div className='vh-9'>
       <div className={styles.ftop}>India</div>
       <div className={styles.fdown}>
-        <div className={styles.fleft}>
+        <div className={styles.fleft} style={{gap:10}}>
           <Link to="/about"><p>About</p></Link>
           <Link to="/advertising"><p>Advertising</p></Link>
           <Link to="/business-profile"><p>Business</p></Link>
           <Link to="/investors"><p>Investor</p></Link>
           <Link to="/hiring"><p>Team</p></Link>
-        </div>
-        <div className={styles.fright}>
           <Link to="/privacy-policy"><p>Privacy & Terms</p></Link>
           <Link to="/help-center"><p>Help</p></Link>
         </div>
