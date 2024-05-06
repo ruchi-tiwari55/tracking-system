4eec3bb2b18f844f9028f01a0fc0c16eb1c6af7b
lzycrazy <87261047+lzycrazy@users.noreply.github.com>
2024-05-06 22:19:31 +0530

    Update SignUp.jsx

--- src/components/common/SignUp.jsx
+++ src/components/common/SignUp.jsx
@@ -29,7 +29,7 @@ function Signup({ toggleSignupForm }) {
     console.log(formData);
     // Send formData to the API
     try {
-      const response = await fetch("https://lzycrazy-tracking-backend-1.onrender.com/v1/users/create", {
+      const response = await fetch("https://lzycrazy-tracking-backend.onrender.com/v1/users/create", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
